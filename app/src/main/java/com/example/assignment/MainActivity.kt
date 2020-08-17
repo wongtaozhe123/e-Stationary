51dfc41a1114d826624a220f3cce23ec068030f4
wongtaozhe123 <wongtaozhelgd@gmail.com>
2020-08-17 17:25:58 +0800

    Commit by Tao Zhe, alpha design done

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -8,10 +8,6 @@ class MainActivity : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        btnRegistrationTemp.setOnClickListener{
-//            val intent=Intent(this,Register::class.java)
-//            startActivity(intent)
-//        }
     }
 
 }
