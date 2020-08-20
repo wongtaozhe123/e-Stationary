3149bf08a87fd5bf2eabf18e3d2f6a686b9ad91c
wongtaozhe123 <wongtaozhelgd@gmail.com>
2020-08-20 14:29:44 +0800

    Commit by Tao Zhe, Kt file created for all

---
+++ app/src/main/java/com/example/assignment/AddToCart.kt
@@ -0,0 +1,16 @@
+package com.example.assignment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.register.*
+
+class AddToCart:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_to_cart)
+    }
+}
