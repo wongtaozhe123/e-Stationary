6a0a4713ab3d836cd95117e90ce52845223c0ec8
parnikakaur22 <parnikakaur673@gmail.com>
2020-09-14 17:38:56 +0800

    settings and tab fragments updates

--- app/src/main/java/com/example/assignment/Settings.kt
+++ app/src/main/java/com/example/assignment/Settings.kt
@@ -1,15 +1,43 @@
 package com.example.assignment
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.register.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.settings.*
+
 
 class Settings:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings)
+
+        var preferences: SharedPreferences = getSharedPreferences("checkbox", MODE_PRIVATE)
+        var checkbox: String? = preferences.getString("remember","")
+
+        getUserProfile()
+
+        btnLogout.setOnClickListener(){
+            var preferences: SharedPreferences = getSharedPreferences("checkbox", MODE_PRIVATE)
+            var editor: SharedPreferences.Editor = preferences.edit()
+            editor.putString("remember","false")
+            editor.apply()
+
+            finish()
+        }
+    }
+
+    private fun getUserProfile() {
+        // [START get_user_profile]
+        val user = FirebaseAuth.getInstance().currentUser
+
+        user?.let {
+            // Name, email address, and profile photo Url
+            val uid = user.uid
+            displayName.setText(user.displayName)
+            displayEmail.setText(user.email)
+            displayPhoneNo.setText(user.phoneNumber)
+        }
+        // [END get_user_profile]
     }
 }
